3ba739447b567d6294e604d2f3b83bcf8508d90e
rkerner <rkerner.mobil@gmail.com>
2021-06-08 18:55:02 +0200

    MGDSTRM-3321 Provide a Connector Fleet Catalog for Debezium
    relates to https://issues.redhat.com/browse/MGDSTRM-3321

--- src/test/java/io/debezium/fleet_catalog/NativeDebeziumConnectorTypeResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.debezium.fleet_catalog;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeDebeziumConnectorTypeResourceIT extends DebeziumConnectorTypeResourceTest {
-
-    // Execute the same tests but in native mode.
-}
